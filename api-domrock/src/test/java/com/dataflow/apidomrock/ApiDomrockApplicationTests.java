bd436a0113b98c80779c4508852ede681cb6a5b5
JoaoMatheusLamao <joaomatheuslamao9@gmail.com>
2024-03-20 22:01:44 -0300

    feat: inits do projeto

---
+++ api-domrock/src/test/java/com/dataflow/apidomrock/ApiDomrockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dataflow.apidomrock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDomrockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
