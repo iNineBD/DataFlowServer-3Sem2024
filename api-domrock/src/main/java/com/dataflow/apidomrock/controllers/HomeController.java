06a96f3675b054253d3e0cf34ede93ae88112642
Andre Bernardes <andreluiz1088@hotmail.com>
2024-04-03 20:15:04 -0300

    fix: exibição da organização no response da tela home

--- api-domrock/src/main/java/com/dataflow/apidomrock/controllers/HomeController.java
+++ api-domrock/src/main/java/com/dataflow/apidomrock/controllers/HomeController.java
@@ -9,10 +9,7 @@ import com.dataflow.apidomrock.entities.database.Usuario;
 import com.dataflow.apidomrock.services.HomeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -20,6 +17,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/home")
+@CrossOrigin
 public class HomeController {
     @Autowired
     HomeService homeService;
--- api-domrock/src/main/java/com/dataflow/apidomrock/dto/ArquivoDTO.java
+++ api-domrock/src/main/java/com/dataflow/apidomrock/dto/ArquivoDTO.java
@@ -5,9 +5,11 @@ import com.dataflow.apidomrock.entities.database.Status;
 import com.fasterxml.jackson.annotation.JsonAlias;
 
 public record ArquivoDTO(@JsonAlias("nomeArquivo")String nome,
+
+                         @JsonAlias("organizacao") String organizacao,
                          @JsonAlias("status") String status) {
 
     public ArquivoDTO(Arquivo arquivo){
-        this(arquivo.getNomeArquivo(), arquivo.getStatus().getStatus());
+        this(arquivo.getNomeArquivo(),arquivo.getOrganizacao().getNome(), arquivo.getStatus().getStatus());
     }
 }
