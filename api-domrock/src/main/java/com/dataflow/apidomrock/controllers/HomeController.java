a553fb280f38ebde0f33b47fdf749ac3349640dd
JoaoMatheusLamao <joaomatheuslamao9@gmail.com>
2024-04-10 21:20:02 -0300

    fix: method http and endpoint files/find

--- api-domrock/src/main/java/com/dataflow/apidomrock/controllers/HomeController.java
+++ api-domrock/src/main/java/com/dataflow/apidomrock/controllers/HomeController.java
@@ -1,14 +1,14 @@
 package com.dataflow.apidomrock.controllers;
 
-import com.dataflow.apidomrock.dto.homedados.ResponseArquivosDTO;
-import com.dataflow.apidomrock.dto.homedados.RequestBodyUsuarioDTO;
-import com.dataflow.apidomrock.dto.homedados.ResponseHomeDTO;
 import com.dataflow.apidomrock.dto.customresponse.ResponseCustomDTO;
+import com.dataflow.apidomrock.dto.homedados.RequestBodyUsuarioDTO;
+import com.dataflow.apidomrock.dto.homedados.ResponseArquivosDTO;
+import com.dataflow.apidomrock.dto.homedados.ResponseHomeDTO;
 import com.dataflow.apidomrock.entities.database.Arquivo;
 import com.dataflow.apidomrock.services.HomeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +22,7 @@ public class HomeController {
     @Autowired
     HomeService homeService;
 
-    @GetMapping("/filesOrg")
+    @PostMapping("/files/find")
     public ResponseEntity<ResponseCustomDTO<ResponseHomeDTO>> homeDados(@RequestBody RequestBodyUsuarioDTO usuario) throws IOException {
         List<Arquivo> arquivos =  homeService.getUsuario(usuario.email());
         List<ResponseArquivosDTO> arquivosLanding = homeService.arquivosLanding(arquivos);
