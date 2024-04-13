92422515cfc3a11f4091be833aa6a6567ea3317d
JoaoMatheusLamao <joaomatheuslamao9@gmail.com>
2024-04-13 14:52:45 -0300

    chore: update imports cors

--- api-domrock/src/main/java/com/dataflow/apidomrock/configuration/CorsAPI.java
+++ api-domrock/src/main/java/com/dataflow/apidomrock/configuration/CorsAPI.java
@@ -1,6 +1,5 @@
 package com.dataflow.apidomrock.configuration;
 
-import org.springframework.context.annimport org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -32,4 +31,4 @@ public class CorsAPI {
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
-}otation.Bean;
+}
