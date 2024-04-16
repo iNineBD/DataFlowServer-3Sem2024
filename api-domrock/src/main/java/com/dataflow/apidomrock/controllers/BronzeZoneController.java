4b2b856ee28ed77fed1a9891b3cf7355740c7601
Andre Bernardes <andreluiz1088@hotmail.com>
2024-04-16 19:01:16 -0300

    feat: criação do controller da etapa bronze e inserção da anotação crossOrigin no homeController.

---
+++ api-domrock/src/main/java/com/dataflow/apidomrock/controllers/BronzeZoneController.java
@@ -0,0 +1,11 @@
+package com.dataflow.apidomrock.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/bronze")
+@CrossOrigin
+public class BronzeZoneController {
+}
--- api-domrock/src/main/java/com/dataflow/apidomrock/controllers/HomeController.java
+++ api-domrock/src/main/java/com/dataflow/apidomrock/controllers/HomeController.java
@@ -17,6 +17,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/home")
+@CrossOrigin("*")
 public class HomeController {
     @Autowired
     HomeService homeService;
