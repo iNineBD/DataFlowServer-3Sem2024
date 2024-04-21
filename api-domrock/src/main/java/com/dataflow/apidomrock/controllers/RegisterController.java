ab71ccfc5075f86809d2d3243caa5ceea4db0539
Ana Raquel <anaraquelysmachado29@gmail.com>
2024-04-21 20:19:51 -0300

    feat: create controller for register

---
+++ api-domrock/src/main/java/com/dataflow/apidomrock/controllers/RegisterController.java
@@ -0,0 +1,24 @@
+package com.dataflow.apidomrock.controllers;
+
+import com.dataflow.apidomrock.controllers.exceptions.CustomException;
+import com.dataflow.apidomrock.dto.customresponse.ResponseCustomDTO;
+import com.dataflow.apidomrock.dto.registerdto.UsuarioDTO;
+import com.dataflow.apidomrock.services.RegisterServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/register")
+@CrossOrigin("*")
+public class RegisterController {
+    @Autowired
+    RegisterServices registerServices;
+    @PostMapping("/cadastro")
+    public ResponseEntity<ResponseCustomDTO<String>> registerUserInDataBase(@RequestBody UsuarioDTO usuarioDTO) throws CustomException{
+        registerServices.registerInDatabase(usuarioDTO);
+        return ResponseEntity.ok().body(new ResponseCustomDTO<>("Processamento efetuado com sucesso", null));
+    }
+
+
+}
