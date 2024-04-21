ab89fcabb467ed639c16ffd3e729a64b43ac76a8
Ana Raquel <anaraquelysmachado29@gmail.com>
2024-04-21 20:26:12 -0300

    Refactor: select by nivelAcesso

--- api-domrock/src/main/java/com/dataflow/apidomrock/repository/NivelAcessoRepository.java
+++ api-domrock/src/main/java/com/dataflow/apidomrock/repository/NivelAcessoRepository.java
@@ -2,8 +2,13 @@ package com.dataflow.apidomrock.repository;
 
 import com.dataflow.apidomrock.entities.database.NivelAcesso;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface NivelAcessoRepository extends JpaRepository<NivelAcesso, Integer> {
+    @Query("SELECT na FROM NivelAcesso na WHERE na.nivel = 'master'")
+    Optional<NivelAcesso> findByTipo();
 }
