bc6516ab655f08b9d7a8d52e0ce3612a82443382
Ana Raquel <anaraquelysmachado29@gmail.com>
2024-04-21 22:25:20 -0300

    fix: changing the select of nivelAcesso repository

--- api-domrock/src/main/java/com/dataflow/apidomrock/repository/NivelAcessoRepository.java
+++ api-domrock/src/main/java/com/dataflow/apidomrock/repository/NivelAcessoRepository.java
@@ -1,6 +1,7 @@
 package com.dataflow.apidomrock.repository;
 
 import com.dataflow.apidomrock.entities.database.NivelAcesso;
+import org.hibernate.annotations.processing.Find;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -9,6 +10,6 @@ import java.util.Optional;
 
 @Repository
 public interface NivelAcessoRepository extends JpaRepository<NivelAcesso, Integer> {
-    @Query("SELECT na FROM NivelAcesso na WHERE na.nivel = 'master'")
-    Optional<NivelAcesso> findByTipo();
+    Optional<NivelAcesso> findByNivel(String nivel);
+
 }
