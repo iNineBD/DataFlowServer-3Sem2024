7fc05dd268f065a266700a18d7c485bc2f691265
Ana Raquel <anaraquelysmachado29@gmail.com>
2024-04-28 23:08:40 -0300

    Fix: changing FetchType

--- api-domrock/src/main/java/com/dataflow/apidomrock/entities/database/Usuario.java
+++ api-domrock/src/main/java/com/dataflow/apidomrock/entities/database/Usuario.java
@@ -32,7 +32,7 @@ public class Usuario implements UserDetails {
     @JoinColumn(name = "cnpj_organizacao", nullable = false)
     private Organizacao organizacao;
 
-    @ManyToMany(targetEntity = NivelAcesso.class, fetch = FetchType.LAZY)
+    @ManyToMany(targetEntity = NivelAcesso.class, fetch = FetchType.EAGER)
     @JoinTable(name = "nivel_acesso_usuario", // nome da tabela no sql
             joinColumns = @JoinColumn(name = "id_usuario"), // fk do usuario na nivel_acesso_usuario
             inverseJoinColumns = @JoinColumn(name = "id_nivel") // fk do nivel na nivel_acesso_usuario
