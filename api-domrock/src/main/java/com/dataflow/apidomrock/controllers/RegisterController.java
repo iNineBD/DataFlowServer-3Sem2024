175cf94e8fc35a903d7c1637271d8880a0a6051b
Ana Raquel <anaraquelysmachado29@gmail.com>
2024-04-28 23:12:52 -0300

    Fix: adding TokenService in RegisterController

--- api-domrock/src/main/java/com/dataflow/apidomrock/controllers/RegisterController.java
+++ api-domrock/src/main/java/com/dataflow/apidomrock/controllers/RegisterController.java
@@ -5,7 +5,9 @@ import com.dataflow.apidomrock.dto.customresponse.ResponseCustomDTO;
 import com.dataflow.apidomrock.dto.registerdto.AutenticacaoDTO;
 import com.dataflow.apidomrock.dto.registerdto.UsuarioDTO;
 import com.dataflow.apidomrock.dto.registerdto.ValidacaoDTO;
+import com.dataflow.apidomrock.entities.database.Usuario;
 import com.dataflow.apidomrock.services.RegisterServices;
+import com.dataflow.apidomrock.services.utils.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -23,6 +25,8 @@ public class RegisterController {
     RegisterServices registerServices;
     @Autowired
     AuthenticationManager authenticationManager;
+    @Autowired
+    private TokenService tokenService;
 
 
     @PostMapping("/cadastro")
@@ -41,6 +45,7 @@ public class RegisterController {
     public ResponseEntity<ResponseCustomDTO<String>> userLogin(@RequestBody @Validated AutenticacaoDTO autenticacaoDTO) throws CustomException, CustomException {
         var usernamePassword = new UsernamePasswordAuthenticationToken(autenticacaoDTO.getLogin(), autenticacaoDTO.getSenha());
         var auth = this.authenticationManager.authenticate(usernamePassword);
-        return ResponseEntity.ok().body(new ResponseCustomDTO<>("Processamento efetuado com sucesso", null));
+        var token = tokenService.generateToken((Usuario) auth.getPrincipal());
+        return ResponseEntity.ok().body(new ResponseCustomDTO<>("Processamento efetuado com sucesso", token));
     }
 }
