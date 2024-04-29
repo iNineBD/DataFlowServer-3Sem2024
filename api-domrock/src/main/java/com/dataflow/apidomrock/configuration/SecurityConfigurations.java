a7d61aaa1e5b91999afe70a4213006655a39ed52
Ana Raquel <anaraquelysmachado29@gmail.com>
2024-04-28 23:06:58 -0300

    Feat: adding SecurityFilter

--- api-domrock/src/main/java/com/dataflow/apidomrock/configuration/SecurityConfigurations.java
+++ api-domrock/src/main/java/com/dataflow/apidomrock/configuration/SecurityConfigurations.java
@@ -1,5 +1,6 @@
 package com.dataflow.apidomrock.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.auditing.config.AuditingConfiguration;
@@ -12,20 +13,23 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
+    @Autowired
+    SecurityFilter securityFilter;
     @Bean
     public SecurityFilterChain securityFilterChain (HttpSecurity httpSecurity) throws Exception{
         return httpSecurity
                 .csrf(crsf -> crsf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.POST, "register/cadastro").permitAll()
                         .requestMatchers(HttpMethod.POST, "register/validacao").permitAll()
                         .requestMatchers(HttpMethod.POST, "register/login").permitAll()
                         .anyRequest().authenticated())
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
     @Bean
