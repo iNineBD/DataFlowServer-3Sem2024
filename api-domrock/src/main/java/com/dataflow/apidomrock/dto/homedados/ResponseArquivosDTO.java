251b15c3cb6569512c3975781cab24a8d1cf8633
JoaoMatheusLamao <joaomatheuslamao9@gmail.com>
2024-05-14 22:06:22 -0300

    fix: corrigindo DTO HOME

--- api-domrock/src/main/java/com/dataflow/apidomrock/dto/homedados/ResponseArquivosDTO.java
+++ api-domrock/src/main/java/com/dataflow/apidomrock/dto/homedados/ResponseArquivosDTO.java
@@ -6,10 +6,10 @@ import com.fasterxml.jackson.annotation.JsonAlias;
 public record ResponseArquivosDTO(@JsonAlias("nomeArquivo")String nome,
 
                                   @JsonAlias("organizacao") String organizacao,
-                                  @JsonAlias("organizacao") String cnpj,
+                                  @JsonAlias("cnpj") String cnpj,
                                   @JsonAlias("status") String status) {
 
     public ResponseArquivosDTO(Arquivo arquivo){
-        this(arquivo.getNomeArquivo(),arquivo.getOrganizacao().getNome(), arquivo.getOrganizacao().getNome(), arquivo.getStatus());
+        this(arquivo.getNomeArquivo(),arquivo.getOrganizacao().getNome(), arquivo.getOrganizacao().getCnpj(), arquivo.getStatus());
     }
 }
