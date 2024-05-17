bfa3fd2eeda7fcb8944a32d05df5733dbcf0e213
Caio Sousa <caio.sousa5@fatec.sp.gov.br>
2024-05-17 19:42:47 -0300

    doc: fixed OpenApiConfig Anotations

--- api-domrock/src/main/java/com/dataflow/apidomrock/configuration/OpenApiConfig.java
+++ api-domrock/src/main/java/com/dataflow/apidomrock/configuration/OpenApiConfig.java
@@ -7,7 +7,7 @@ import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
-@OpenAPIDefinition(info = @Info(title = "DomRock API", version = "v1", description = "API para gerenciamento de pipeline de dados", contact = @Contact(name = "DomRock")))
+@OpenAPIDefinition(info = @Info(title = "DomRock Dataflow Project", version = "v1", description = "Projeto integrador de gerenciamento de pipeline de dados", contact = @Contact(name = "iNine", url = "https://github.com/iNineBD")))
 
 @SecurityScheme(name = "bearerAuth", description = "JWT auth description", scheme = "bearer", type = SecuritySchemeType.HTTP, bearerFormat = "JWT", in = SecuritySchemeIn.HEADER)
 public class OpenApiConfig {
