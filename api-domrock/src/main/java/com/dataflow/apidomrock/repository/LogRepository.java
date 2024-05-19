0007ed4f4e023fe039e6a819423c92ee6ffedfd0
Ana Raquel <anaraquelysmachado29@gmail.com>
2024-05-19 01:29:11 -0300

    Feat: adding findByArquivo

--- api-domrock/src/main/java/com/dataflow/apidomrock/repository/LogRepository.java
+++ api-domrock/src/main/java/com/dataflow/apidomrock/repository/LogRepository.java
@@ -1,13 +1,17 @@
 package com.dataflow.apidomrock.repository;
 
+import com.dataflow.apidomrock.entities.database.Arquivo;
 import com.dataflow.apidomrock.entities.database.Log;
 import com.dataflow.apidomrock.entities.database.Metadata;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface LogRepository extends JpaRepository<Log, Integer> {
     @Query(value = "select * from log where acao = :acao and estagio = :estagio and id_arquivo = :fileId order by data_hora desc limit 1", nativeQuery = true)
     Optional<Log> findLastObsInFile(Integer fileId, String estagio, String acao);
+
+    List<Log> findByArquivo(Arquivo arquivo);
 }
