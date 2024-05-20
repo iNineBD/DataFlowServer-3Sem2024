33c826157bb25c2395ebbf857674143fc45e7aa5
Caio Sousa <caio.sousa5@fatec.sp.gov.br>
2024-05-20 19:52:30 -0300

    DOC: adicionada documentação DTOs Create hash

--- api-domrock/src/main/java/com/dataflow/apidomrock/controllers/BronzeZoneController.java
+++ api-domrock/src/main/java/com/dataflow/apidomrock/controllers/BronzeZoneController.java
@@ -1,6 +1,6 @@
 package com.dataflow.apidomrock.controllers;
 
-//internal imports
+//java imports
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
--- api-domrock/src/main/java/com/dataflow/apidomrock/dto/createHash/RequestArquivoDTO.java
+++ api-domrock/src/main/java/com/dataflow/apidomrock/dto/createHash/RequestArquivoDTO.java
@@ -7,7 +7,8 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "DTO para requisição de criação de hash")
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record RequestArquivoDTO(@JsonAlias("usuario") String usuario,
-                @JsonAlias("arquivo") String nomeArquivo,
-                @JsonAlias("cnpj") String cnpj) {
+public record RequestArquivoDTO(
+        @JsonAlias("usuario") @Schema(description = "Nome do usuário", example = "User") String usuario,
+        @JsonAlias("arquivo") @Schema(description = "nome do arquivo", example = "Arquivo 1") String nomeArquivo,
+        @JsonAlias("cnpj") @Schema(description = "Cnpj da instituição responsável pelo arquivo", example = "99.205.190/0001-40") String cnpj) {
 }
