d6f72004bbe3d684be24f69ee8c50cc5ac264d2b
Caio Sousa <caio.sousa5@fatec.sp.gov.br>
2024-05-21 22:02:14 -0300

    DOC: MetadadosDePara, RequestMetaToDePara, ResponseMetaToDePara had received doc

--- api-domrock/src/main/java/com/dataflow/apidomrock/dto/getmetadadostotepara/RequestMetaToDePara.java
+++ api-domrock/src/main/java/com/dataflow/apidomrock/dto/getmetadadostotepara/RequestMetaToDePara.java
@@ -1,13 +1,17 @@
 package com.dataflow.apidomrock.dto.getmetadadostotepara;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import java.util.List;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record RequestMetaToDePara(@JsonAlias("email") String email,
-                                  @JsonAlias("arquivo") String arquivo,
-                                  @JsonAlias("cnpj") String cnpj,
-                                  @JsonAlias("metadados")List<MetadadosDePara> listMetadados) {
+@Schema(name = "RequestMetaToDePara", description = "Request para buscar metadados de um de/para")
+public record RequestMetaToDePara(
+        @Schema(description = "email do responsável pelo arquivo", example = "user@gmail.com") @JsonAlias("email") String email,
+        @Schema(description = "Nome do arquivo em questão", example = "arquivo01") @JsonAlias("arquivo") String arquivo,
+        @Schema(description = "Cnpj da organização em responsável pelo arquivo", example = "99.205.190/0001-40") @JsonAlias("cnpj") String cnpj,
+        @Schema(description = "lista de metadados referentes ao arqivo", example = "coluna01,coluna02") @JsonAlias("metadados") List<MetadadosDePara> listMetadados) {
 }
