57e6e40d564a881358799be85d7eeea481d8a12c
Caio Sousa <caio.sousa5@fatec.sp.gov.br>
2024-05-21 21:03:36 -0300

    DOC: OrganizationDTO, MetricsFilesDTO, RequestFileAnalyticDTO, ResponseFileAnalyticDTO and RequestFileAnalyticDTO had doc implemented

--- api-domrock/src/main/java/com/dataflow/apidomrock/dto/fileAnalytics/RequestFileAnalyticDTO.java
+++ api-domrock/src/main/java/com/dataflow/apidomrock/dto/fileAnalytics/RequestFileAnalyticDTO.java
@@ -1,5 +1,6 @@
 package com.dataflow.apidomrock.dto.fileAnalytics;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,10 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "DTO para requisição de arquivos")
 public class RequestFileAnalyticDTO {
+    @Schema(description = "Tipo de busca", example = "cnpj")
     private String searchType;
+    @Schema(description = "Valor da busca", example = "99.205.190/0001-40")
     private String cnpj;
 }
