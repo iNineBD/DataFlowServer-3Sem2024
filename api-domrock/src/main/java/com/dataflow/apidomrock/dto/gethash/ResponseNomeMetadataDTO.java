d08134ab1b2527a1f2746040ab221e2bd25d907d
Caio Sousa <caio.sousa5@fatec.sp.gov.br>
2024-05-21 21:15:31 -0300

    DOC: criação de documentação para os DTOs ResponseHashToSilverDTO, ResponseNomeMetadataDTO, RequestHashToSilverDTO

--- api-domrock/src/main/java/com/dataflow/apidomrock/dto/gethash/ResponseNomeMetadataDTO.java
+++ api-domrock/src/main/java/com/dataflow/apidomrock/dto/gethash/ResponseNomeMetadataDTO.java
@@ -4,10 +4,13 @@ import com.dataflow.apidomrock.entities.database.Metadata;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Schema(description = "DTO para resposta de hash para silver")
 public record ResponseNomeMetadataDTO(@JsonAlias("nome") String nome) {
 
-    public ResponseNomeMetadataDTO(Metadata metadata){
+    public ResponseNomeMetadataDTO(Metadata metadata) {
         this(metadata.getNome());
     }
 }
