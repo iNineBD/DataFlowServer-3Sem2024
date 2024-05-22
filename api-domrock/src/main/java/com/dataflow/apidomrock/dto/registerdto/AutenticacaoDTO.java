26d8cf103b8f9a1bde1be34a8dd41389cef7c231
Caio Sousa <caio.sousa5@fatec.sp.gov.br>
2024-05-22 19:50:16 -0300

    DOC: AutenticacaoDTO, ResponseLogin, UsuarioDTO and ValidacaoDTO had doc implemented

--- api-domrock/src/main/java/com/dataflow/apidomrock/dto/registerdto/AutenticacaoDTO.java
+++ api-domrock/src/main/java/com/dataflow/apidomrock/dto/registerdto/AutenticacaoDTO.java
@@ -1,14 +1,22 @@
 package com.dataflow.apidomrock.dto.registerdto;
 
-import lombok.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
-public class AutenticacaoDTO{
+@Schema(name = "AutenticacaoDTO", description = "DTO para autenticação de usuário")
+public class AutenticacaoDTO {
+    @Schema(description = "Login do usuário", example = "user")
     private String login;
+    @Schema(description = "Senha do usuário", example = "123456")
     private String senha;
 
 }
--- api-domrock/src/main/java/com/dataflow/apidomrock/dto/registerdto/ResponseLoginDTO.java
+++ api-domrock/src/main/java/com/dataflow/apidomrock/dto/registerdto/ResponseLoginDTO.java
@@ -1,9 +1,11 @@
 package com.dataflow.apidomrock.dto.registerdto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import io.swagger.v3.oas.annotations.media.Schema;
 
-
-public record ResponseLoginDTO(String token, String nomeUsuario, String email, boolean isMaster) {
+@Schema(name = "ResponseLoginDTO", description = "DTO para resposta de autenticação de usuário")
+public record ResponseLoginDTO(
+        @Schema(description = "Token de autenticação", example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1c2VyIiwiaWF") String token,
+        @Schema(description = "Nome do usuário", example = "Usu ario") String nomeUsuario,
+        @Schema(description = "E-mail do usuário", example = "user@gmail.com") String email,
+        @Schema(description = "Usuário é master", example = "false") boolean isMaster) {
 }
