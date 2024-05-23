87745f4f5d32cd6b97f41c1f3051723939cc297e
Caio Sousa <caio.sousa5@fatec.sp.gov.br>
2024-05-22 21:55:47 -0300

    DOC: DeParasVisualize, MetadadosDeParaVisualize, RequestDadosToDePara, RequestVisualizeHashDTO, ResponseHashDTO had doc implemented

--- api-domrock/src/main/java/com/dataflow/apidomrock/dto/visualizeDePara/MetadadosDeParaVisualize.java
+++ api-domrock/src/main/java/com/dataflow/apidomrock/dto/visualizeDePara/MetadadosDeParaVisualize.java
@@ -1,15 +1,19 @@
 package com.dataflow.apidomrock.dto.visualizeDePara;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import java.util.List;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record MetadadosDeParaVisualize(@JsonAlias("nome") String nome,
-                                       @JsonAlias("deParas") List<DeParasVisualize> dePara) {
+@Schema(description = "DTO para visualização de metadados deParas")
+public record MetadadosDeParaVisualize(
+        @JsonAlias("nome") @Schema(description = "Nome do arquivo em questão", example = "Arquivo01") String nome,
+        @JsonAlias("deParas") @Schema(description = "Lista de deParas do arquivo em questão", example = "coluna01, coluna02") List<DeParasVisualize> dePara) {
 
-    public MetadadosDeParaVisualize(List<DeParasVisualize> dePara, String nomeMetadado){
+    public MetadadosDeParaVisualize(List<DeParasVisualize> dePara, String nomeMetadado) {
         this(nomeMetadado, dePara);
     }
 }
