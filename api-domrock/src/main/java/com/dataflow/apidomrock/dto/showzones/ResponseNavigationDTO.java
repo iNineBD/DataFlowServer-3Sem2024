23e53c5dfb296297855e82cc93a2f3c55349d235
Caio Sousa <caio.sousa5@fatec.sp.gov.br>
2024-05-22 21:34:38 -0300

    DOC: RequestBodySetStatusBzDTO, RequestBodySetStatusSz, RequestDadosDTO, ResponseNavigationDTO, RequestBodyUpdateMetaDTO had doc implemented

--- api-domrock/src/main/java/com/dataflow/apidomrock/dto/showzones/ResponseNavigationDTO.java
+++ api-domrock/src/main/java/com/dataflow/apidomrock/dto/showzones/ResponseNavigationDTO.java
@@ -3,9 +3,13 @@ package com.dataflow.apidomrock.dto.showzones;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record ResponseNavigationDTO(@JsonAlias("email") String email,
-                                    @JsonAlias("acessoLanding") boolean acessoLanding,
-                                    @JsonAlias("acessoBronze") boolean acessoBronze,
-                                    @JsonAlias("acessoSilver") boolean acessoSilver) {
+@Schema(description = "DTO para response de navegação entre as zonas")
+public record ResponseNavigationDTO(
+        @JsonAlias("email") @Schema(description = "email do usuário navegando as zonas", example = "user@gmail.com") String email,
+        @JsonAlias("acessoLanding") @Schema(description = "boleano para resposta de acesso a navegação do usuário para landing zone", example = "true") boolean acessoLanding,
+        @JsonAlias("acessoBronze") @Schema(description = "boleano para resposta de acesso a navegação do usuário para bronze zone", example = "false") boolean acessoBronze,
+        @JsonAlias("acessoSilver") @Schema(description = "boleano para resposta de acesso a navegação do usuário para silver zone", example = "false") boolean acessoSilver) {
 }
