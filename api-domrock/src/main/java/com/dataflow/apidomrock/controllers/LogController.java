05b73f96bf7381ca0c350750f202fceeddf85e7b
Anaraquely <anaraquelysmachado29@gmail.com>
2024-06-09 19:06:11 -0300

    Feat: implements "visualizarLogUsuario" on Controller

--- api-domrock/src/main/java/com/dataflow/apidomrock/controllers/LogController.java
+++ api-domrock/src/main/java/com/dataflow/apidomrock/controllers/LogController.java
@@ -3,8 +3,10 @@ package com.dataflow.apidomrock.controllers;
 
 import com.dataflow.apidomrock.controllers.exceptions.CustomException;
 import com.dataflow.apidomrock.dto.customresponse.ResponseCustomDTO;
+import com.dataflow.apidomrock.dto.log.RequestLogUsuario;
 import com.dataflow.apidomrock.dto.log.ResponseLog;
 import com.dataflow.apidomrock.dto.log.RequestLogDTO;
+import com.dataflow.apidomrock.dto.log.ResponseLogUsuario;
 import com.dataflow.apidomrock.services.utils.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -22,5 +24,11 @@ public class LogController {
     public ResponseEntity<ResponseCustomDTO<List<ResponseLog>>>visualizarLog(@RequestBody RequestLogDTO requestLogDTO)throws CustomException{
         List<ResponseLog> log = logger.visualizarLog(requestLogDTO);
         return ResponseEntity.ok().body(new ResponseCustomDTO<>("Processamento efetuado com sucesso", log));
+    }
+
+    @PostMapping("/usuario")
+    public  ResponseEntity<ResponseCustomDTO<List<ResponseLogUsuario>>> visualizarLogUsuario (@RequestBody RequestLogUsuario requestLogUsuario) throws CustomException{
+        List<ResponseLogUsuario> logUsuario = logger.visualizarLogUsuario(requestLogUsuario);
+        return ResponseEntity.ok().body(new ResponseCustomDTO<>("Processamento efetuado com sucesso", logUsuario));
     }
 }
