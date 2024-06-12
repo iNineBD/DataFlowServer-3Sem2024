54b2a22cc64483ef81325ee74f91bf3f041f1166
JoaoMatheusLamao <joaomatheuslamao9@gmail.com>
2024-06-12 19:08:00 -0300

    feat: add cnpj in response list all users

--- api-domrock/src/main/java/com/dataflow/apidomrock/dto/log/UserDTO.java
+++ api-domrock/src/main/java/com/dataflow/apidomrock/dto/log/UserDTO.java
@@ -3,6 +3,7 @@ package com.dataflow.apidomrock.dto.log;
 import com.fasterxml.jackson.annotation.JsonAlias;
 
 public record UserDTO(@JsonAlias("organizacao") String organizacao,
+                      @JsonAlias("cnpj") String cnpj,
                       @JsonAlias ("nome") String nome,
                       @JsonAlias("nivel")String nivel,
                       @JsonAlias("email") String email) {
