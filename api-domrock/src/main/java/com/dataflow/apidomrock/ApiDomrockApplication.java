18e08056fc873f721701be88064a37db2c58aab5
JoaoMatheusLamao <joaomatheuslamao9@gmail.com>
2024-06-16 16:01:02 -0300

    feat: the end
    Co-Authored-By: Ana Raquel <127275110+Anaraquely@users.noreply.github.com>
    Co-Authored-By: Andre-Bernardes200 <127262908+Andre-Bernardes200@users.noreply.github.com>
    Co-Authored-By: Beatriz <125521054+BeatrizBonatto@users.noreply.github.com>
    Co-Authored-By: Caio M Sousa <111454312+Caio-sousaFatec@users.noreply.github.com>
    Co-Authored-By: Lucas Henrique Costa de Oliveira <81196630+LucasHCOliveira7@users.noreply.github.com>
    Co-Authored-By: Naiara Santos <127265827+NaiaraSantos3@users.noreply.github.com>
    Co-Authored-By: Eduardo Farias de Paula <117841950+eduardofpaula@users.noreply.github.com>

--- api-domrock/src/main/java/com/dataflow/apidomrock/ApiDomrockApplication.java
+++ api-domrock/src/main/java/com/dataflow/apidomrock/ApiDomrockApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
+/*
+	Olá, caros colegas desenvolvedores!
+	Com este comentário, encerramos a sprint-4 e, consequentemente, o API 3° semestre!!
+	Obrigado a todos os envolvidos!!!
+*/
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "API DomRock", version = "1.0", description = "API realizada em conjunto com a empresa Dom Rock para gerenciamento de pipeline de dados"))
 public class ApiDomrockApplication {
