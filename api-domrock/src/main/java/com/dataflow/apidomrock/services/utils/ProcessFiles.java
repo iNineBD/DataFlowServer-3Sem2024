5a70ae86a6e17232a13e7e64ce39e55a0deddfd6
JoaoMatheusLamao <joaomatheuslamao9@gmail.com>
2024-07-10 21:54:19 -0300

    fix: metadados ativos (upload to silver)

--- api-domrock/src/main/java/com/dataflow/apidomrock/services/utils/ProcessFiles.java
+++ api-domrock/src/main/java/com/dataflow/apidomrock/services/utils/ProcessFiles.java
@@ -214,7 +214,7 @@ public class ProcessFiles {
                 String metadado = content[0].replace("\"","").trim();
                 int qtd = arquivo.get().getMetadados().size();
                 for(int i = 0; i < qtd;i++){
-                    if(metadado.equalsIgnoreCase(arquivo.get().getMetadados().get(i).getNome())){
+                    if(metadado.equalsIgnoreCase(arquivo.get().getMetadados().get(i).getNome())&& arquivo.get().getMetadados().get(i).getIsAtivo()){
                         exist = true;
                     }
                 }
@@ -330,7 +330,7 @@ public class ProcessFiles {
             String metadado = currentRow.getCell(0).getStringCellValue().replace("\"","").trim();
             int qtd = arquivo.get().getMetadados().size();
             for(int j = 0; j < qtd;j++){
-                if(metadado.equalsIgnoreCase(arquivo.get().getMetadados().get(j).getNome())){
+                if(metadado.equalsIgnoreCase(arquivo.get().getMetadados().get(j).getNome())&& arquivo.get().getMetadados().get(i).getIsAtivo()){
                     exist = true;
                 }
             }
@@ -416,7 +416,7 @@ public class ProcessFiles {
             String metadado = currentRow.getCell(0).getStringCellValue().replace("\"","").trim();
             int qtd = arquivo.get().getMetadados().size();
             for(int j = 0; j < qtd;j++){
-                if(metadado.equalsIgnoreCase(arquivo.get().getMetadados().get(j).getNome())){
+                if(metadado.equalsIgnoreCase(arquivo.get().getMetadados().get(j).getNome())&& arquivo.get().getMetadados().get(i).getIsAtivo()){
                     exist = true;
                 }
             }
